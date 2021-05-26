95def0592c16abf848837689fcd4b0e41a7db7e1
bharat-dussa <bharatdussa@gmail.com>
2021-05-26 22:24:23 +0530

    shifting from global to app.css

--- src/styled/Navbar/NavbarContainer.tsx
+++ src/styled/Navbar/NavbarContainer.tsx
@@ -9,8 +9,5 @@ export const NavbarContainer = styled.nav`
         width:100%;
         box-shadow: 0px 3px 28px rgba(0,0,0,0.08);
         margin-bottom:2rem;
-        @media (min-width:768px) {
-            height:15vh;
-        }
     }
 `
